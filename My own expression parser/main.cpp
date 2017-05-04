17206ce1fc761e8de7b3dcc359dec7e8355f000c
Kuska Misha <kushkamisha@yahoo.com>
2017-05-04 17:11:37 +0300

    Expression parser
    Input: 6^2-(5+4)/3
    Output: 33
    
    Allowed: digits (0-9), ‘+’, ‘-‘, ‘*’, ‘/‘, ‘^’, ‘)’, ‘(‘

---
+++ My own expression parser/main.cpp
@@ -0,0 +1,143 @@
+//
+//  main.cpp
+//  My own expression parser
+//
+//  Created by  Roman Usherenko on 03.05.17.
+//  Copyright © 2017 Kushka Misha & Yazenok Misha. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <list>
+#include <map>
+#include <cmath>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+    // Input data
+    string s = "";
+    cout << "Enter expression: ";
+    cin >> s;
+    
+    /**
+     * The shunting algorithm
+     */
+    list<char> stack;
+    list<char> queue;
+    string c = "";
+    map<char, int> precedence = {
+        {'-', 1}, {'+', 1},
+        {'*', 2}, {'/', 2},
+        {'^', 3}
+    };
+    
+    for (int i = 0; i < s.length(); ++i)
+    {
+        if (isdigit(s[i]))
+            // It's a number
+            queue.push_back(s[i]);
+        else
+        {
+            if (s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/' || s[i] == '^')
+            {
+                // It's an operator
+                while (!stack.empty())
+                {
+                    if (precedence[stack.back()] > precedence[s[i]])
+                    {
+                        cout << stack.back() << endl;
+                        queue.push_back(stack.back());
+                        stack.pop_back();
+                    }
+                    else
+                        break;
+                }
+                stack.push_back(s[i]);
+            }
+            else
+            {
+                if (s[i] == '(')
+                    stack.push_back('(');
+                if (s[i] == ')')
+                {
+                    while (!stack.empty())
+                    {
+                        if (stack.back() != '(')
+                        {
+                            queue.push_back(stack.back());
+                            stack.pop_back();
+                        }
+                        else
+                        {
+                            stack.pop_back();
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    // While there's operators on the stack, pop them to the queue
+    while (!stack.empty())
+    {
+        queue.push_back(stack.back());
+        stack.pop_back();
+    }
+    
+    // output queue
+    copy(queue.begin(), queue.end(), ostream_iterator<char>(cout, " "));
+    cout << endl;
+    
+    /**
+     * Reverse polish
+     */
+    double a, b, result = 0;
+    list<double>fstack;
+    
+    for (list<char>::iterator p = queue.begin(); p != queue.end(); ++p)
+    {
+        if (isdigit(*p))
+            fstack.push_back(*p - '0');
+        else
+        {
+            cout << "before: ";
+            copy(fstack.begin(), fstack.end(), ostream_iterator<double>(cout, " "));
+            cout << endl;
+            a = fstack.back();
+            fstack.pop_back();
+            b = fstack.back();
+            fstack.pop_back();
+            cout << "a = " << a << endl;
+            cout << "b = " << b << endl;
+            switch (*p)
+            {
+                case '+':
+                    result = b + a;
+                    break;
+                case '-':
+                    result = b - a;
+                    break;
+                case '*':
+                    result = b * a;
+                    break;
+                case '/':
+                    result = (double) b / a;
+                    break;
+                case '^':
+                    result = (double) pow((double) b, a);
+                    break;
+            }
+            cout << "result = " << result << endl;
+            fstack.push_back(result);
+            cout << "after: ";
+            copy(fstack.begin(), fstack.end(), ostream_iterator<double>(cout, " "));
+            cout << endl;
+        }
+    }
+    copy(fstack.begin(), fstack.end(), ostream_iterator<double>(cout, " "));
+    cout << endl;
+}
+
+// 4*8/(9-5)
